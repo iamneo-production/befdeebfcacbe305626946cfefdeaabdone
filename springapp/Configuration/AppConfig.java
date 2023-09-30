3c244b0cb5ea579e247bfdbe6ea8289cb34e83aa
Iamneo <techadmin@iamneo.ai>
2023-09-30 15:04:10 +0000

    Backup Commit

--- springapp/Configuration/AppConfig.java
+++ springapp/Configuration/AppConfig.java
@@ -1,3 +1,10 @@
 public class AppConfig {
+    public Student student(){
+        Student student=new Student();
+        student.setStudentId(1);
+        student.setFirstName("John");
+        student.setLastName("Doe");
+        return student;
+    }
     
 }
